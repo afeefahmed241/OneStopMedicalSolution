fe06a11691a6d3a59990186f3182ea4a8cc31b80
afeefahmed <afeefahmed@iut-dhaka.edu>
2021-01-04 00:10:40 +0600

    Adding the Login ,SignUp and ForgotPassword Feature

--- app/src/main/java/com/example/onestopmedicalsolution/MainActivity.java
+++ app/src/main/java/com/example/onestopmedicalsolution/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.onestopmedicalsolution;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView btnSignUp;
     private TextView btnLogin;
     private TextView btnForgotPass;
+    private EditText edtEmail,edtPass;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
         btnSignUp = findViewById(R.id.tvSignIN);
         btnLogin = findViewById(R.id.tvLogin);
         btnForgotPass = findViewById(R.id.tvForgotPass);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPass = findViewById(R.id.edtPass);
+
+        auth = FirebaseAuth.getInstance();
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,14 +48,62 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openManuActivity();
+                String email,pass;
+
+                email = edtEmail.getText().toString().trim();
+                pass = edtPass.getText().toString();
+
+                if(email.isEmpty() || pass.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "Fill Up all the Fields", Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                    auth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                            {
+                                Toast.makeText(MainActivity.this, "Logged IN", Toast.LENGTH_SHORT).show();
+                                openManuActivity();
+                            }
+                            else
+                            {
+                                Toast.makeText(MainActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }
+
+
             }
         });
 
         btnForgotPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openForgotPasswordActivity();
+                String email;
+                email = edtEmail.getText().toString().trim();
+
+                if(email.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "Please provide an Email", Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                    auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "A Reset Password Email has been sent", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(MainActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }
 
             }
         });
@@ -48,10 +111,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void openForgotPasswordActivity() {
-        Intent intent =  new Intent(this,ForgotPassword.class);
-        startActivity(intent);
-    }
+
 
     private void openManuActivity() {
         Intent intent = new Intent(this,ManuActivity.class);
