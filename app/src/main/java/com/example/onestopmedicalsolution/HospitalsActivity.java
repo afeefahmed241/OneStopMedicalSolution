e2413a6869278d4f6b9517d6b819aa9c472a4f29
afeefahmed <afeefahmed@iut-dhaka.edu>
2021-01-05 00:47:30 +0600

    Adding the Hospital List RecyclerView

--- app/src/main/java/com/example/onestopmedicalsolution/HospitalsActivity.java
+++ app/src/main/java/com/example/onestopmedicalsolution/HospitalsActivity.java
@@ -1,33 +1,65 @@
 package com.example.onestopmedicalsolution;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class HospitalsActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    private TextView btnTstBtnH;
+public class HospitalsActivity extends AppCompatActivity implements HospitalAdapter.ItemClicked {
+
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter myAdapter;
+    RecyclerView.LayoutManager layoutManager;
+
+    ArrayList<Hospital> hospitals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hospitals);
 
-        btnTstBtnH = findViewById(R.id.tvTstBtnH);
+        recyclerView = findViewById(R.id.listHospitals);
+        recyclerView.setHasFixedSize(true);
 
-        btnTstBtnH.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openHospitalInfoActivity();
-            }
-        });
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        hospitals = new ArrayList<Hospital>();
+
+        hospitals.add(new Hospital("Kurmitola","0171329847","231"));
+        hospitals.add(new Hospital("DMC","0171329847","241"));
+        hospitals.add(new Hospital("CMH","0171329847","250"));
+        hospitals.add(new Hospital("SJDF","0171329847","221"));
+        hospitals.add(new Hospital("Kurmitola","0171329847","231"));
+        hospitals.add(new Hospital("DMC","0171329847","241"));
+        hospitals.add(new Hospital("CMH","0171329847","250"));
+        hospitals.add(new Hospital("SJDF","0171329847","221"));
+        hospitals.add(new Hospital("Kurmitola","0171329847","231"));
+        hospitals.add(new Hospital("DMC","0171329847","241"));
+        hospitals.add(new Hospital("CMH","0171329847","250"));
+        hospitals.add(new Hospital("SJDF","0171329847","221"));
+
+
+        // for updating the listView myAdapter.notifyDataSetChanged();
+
+
+        myAdapter = new HospitalAdapter(this,hospitals);
+        recyclerView.setAdapter(myAdapter);
     }
 
-    private void openHospitalInfoActivity() {
-        Intent intent = new Intent(this,HospitalInfo.class);
+
+    @Override
+    public void onItemClicked(int index) {
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q="+hospitals.get(index).getName()));
         startActivity(intent);
     }
 }
